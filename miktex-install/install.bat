04856ad190d92a2b2afb5ee1a3951281849e716e
Malte Breuer <malte.breuer@gmail.com>
2019-03-01 14:01:11 +0100

    Referenced correct MiKTeX portable install file

--- miktex-install/install.bat
+++ miktex-install/install.bat
@@ -2,7 +2,7 @@ REM @echo off
 Pushd "%~dp0"
 
 echo Installing portable MiKTeX...
-start /b /wait miktex-portable-2.9.6753.exe -o"..\miktex" -y
+start /b /wait miktex-portable-2.9.6942.exe -o"..\miktex" -y
 echo Installation completed.
 
 popd
